dff687e369a05aa66e0b5649cc47bf9efa2049f2
Seu NomeKleber <knpublicidades@hotmail.com>
2025-09-16 11:05:57 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use WandesCardoso\MercadoPago\Facades\MercadoPago;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/teste-mercadopago/{id}', function ($id) {
+    // Busca um pagamento pelo ID
+    $mp = MercadoPago::payment()->find($id);
+
+    // Mostra o resultado na tela
+    dd($mp); // ou var_dump($mp)
+    /*echo '<pre>';
+print_r($mp);
+echo '</pre>';*/
+
+});
